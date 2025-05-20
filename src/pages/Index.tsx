e185e42568e3bffc162234ba582cfdf65ae5faeb
Richie <whazlewo@gmail.com>
2025-05-20 14:15:23 -0700

    Adding Canvas Kit Dialog with Buttons

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,9 @@
-import { PrimaryButton } from "@workday/canvas-kit-react/button";
+import { PrimaryButton, SecondaryButton } from "@workday/canvas-kit-react/button";
+import { Dialog } from "@workday/canvas-kit-react/dialog";
 import { Flex } from "@workday/canvas-kit-react/layout";
-import { plusIcon, relatedActionsVerticalIcon, caretDownIcon } from "@workday/canvas-system-icons-web";
 import { createStyles } from "@workday/canvas-kit-styling";
 import { system } from "@workday/canvas-tokens-web";
+import { useState } from "react";
 
 const parentContainerStyles = createStyles({
   gap: system.space.x4,
@@ -10,16 +11,28 @@ const parentContainerStyles = createStyles({
 });
 
 const CKTest = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => setIsOpen(true);
+  const handleClose = () => setIsOpen(false);
+
   return (
     <Flex cs={parentContainerStyles}>
-      <PrimaryButton>Primary</PrimaryButton>
-      <PrimaryButton icon={plusIcon} iconPosition="start">
-        Primary
-      </PrimaryButton>
-      <PrimaryButton icon={caretDownIcon} iconPosition="end">
-        Primary
-      </PrimaryButton>
-      <PrimaryButton aria-label="Related Actions" icon={relatedActionsVerticalIcon} />
+      <PrimaryButton onClick={handleOpen}>Save</PrimaryButton>
+      <SecondaryButton onClick={handleClose}>Cancel</SecondaryButton>
+
+      <Dialog isOpen={isOpen} onDismiss={handleClose}>
+        <Dialog.Target>
+          <Dialog.Title>Confirm Action</Dialog.Title>
+          <Dialog.Body>Are you sure you want to save your changes?</Dialog.Body>
+          <Dialog.Footer>
+            <Flex gap="s">
+              <PrimaryButton onClick={handleClose}>Continue</PrimaryButton>
+              <SecondaryButton onClick={handleClose}>Cancel</SecondaryButton>
+            </Flex>
+          </Dialog.Footer>
+        </Dialog.Target>
+      </Dialog>
     </Flex>
   );
 };
